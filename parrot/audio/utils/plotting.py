28557f4b5a190c90a17a68ea580d7621276786b2
Federico Minutoli <f.minutoli@reply.it>
2023-11-16 20:48:28 +0100

    add plotting extra and utility

---
+++ parrot/audio/utils/plotting.py
@@ -0,0 +1,32 @@
+"""A utility module for plotting audio signals"""
+# pylint: disable=C0103
+import pydub
+
+
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    has_plotting = True
+except ImportError:
+    has_plotting = False
+
+
+def plot_segment(segment: pydub.AudioSegment) -> None:
+    """It plots the amplitude of a segment over time"""
+    if not has_plotting:
+        message = "parrot[plotting] must be installed"
+        raise ValueError(message)
+
+    T = segment.duration_seconds
+    f = segment.frame_rate
+
+    t = np.arange(0, T, 1 / f)
+    x = segment.get_array_of_samples()
+
+    plt.plot(t, x)
+
+    plt.xlabel("time [s]")
+    plt.ylabel("amplitude")
+
+    plt.show()
